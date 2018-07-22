d19c3c61766c7ce548b723fa17affed9483629a6
Hamish <hamishfpso@gmail.com>
2018-07-22 21:24:18 +0100

    Added photo to users dashboard

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -55,7 +55,8 @@ class AdminUsersController extends Controller
         }
         $input['password'] = bcrypt($request->password);
         User::create($input);
-        // return redirect('/admin/users');
+
+        return redirect('/admin/users');
 
         // return $request->all();
     }
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,6 +9,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -22,6 +23,7 @@
         @foreach($users as $user)
           <tr>
             <td>{{$user->id}}</td>
+            <td><img height="50" src="{{$user->photo ? $user->photo->file : 'No user photo'}}"></td>
             <td>{{$user->name}}</td>
             <td>{{$user->email}}</td>
             <td>{{$user->role->name}}</td>
